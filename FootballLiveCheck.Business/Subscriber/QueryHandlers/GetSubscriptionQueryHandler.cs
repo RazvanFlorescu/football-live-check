3a8ddac5cf0b4a70daea69e1bc99a590770d7dff
robertbutacu <robert.butacu@yahoo.com>
2018-01-13 21:27:00 +0200

    added Query so now a subscription won't be added twice

---
+++ FootballLiveCheck.Business/Subscriber/QueryHandlers/GetSubscriptionQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿
+
+using AutoMapper;
+using EnsureThat;
+using FootballLiveCheck.Business.Subscriber.Models;
+using FootballLiveCheck.Business.Subscriber.Queries;
+using FootballLiveCheck.Business.Subscriber.QueryResults;
+using FootballLiveCheck.CqrsCore.Queries;
+using FootballLiveCheck.Domain.Repositories;
+using System.Linq;
+
+namespace FootballLiveCheck.Business.Subscriber.QueryHandlers
+{
+    public class GetSubscriptionQueryHandler : DatabaseHandler, IQueryHandler<GetSubscriptionQuery, GetSubscriptionQueryResult>
+    {
+        private readonly ISubscriberRepository SubscriptionsRepo;
+
+        public GetSubscriptionQueryHandler(IMapper mapper, ISubscriberRepository subsRepo) : base(mapper)
+        {
+            EnsureArg.IsNotNull(subsRepo);
+            this.SubscriptionsRepo = subsRepo;
+        }
+
+        public GetSubscriptionQueryResult Retrieve(GetSubscriptionQuery query)
+        {
+            EnsureArg.IsNotNull(query);
+
+            var subscriptionsBySamePhoneNumber = SubscriptionsRepo.Search(s => s.PhoneNumber == query.Subscription.PhoneNumber);
+            var matches = Mapper.Map<IQueryable<SubscriptionModel>>(subscriptionsBySamePhoneNumber);
+            matches.Where(s => s.SubcribedGame == query.Subscription.SubcribedGame);
+
+            return new GetSubscriptionQueryResult(matches);
+        }
+    }
+}
--- FootballLiveCheck.Service/Controllers/SubscribersController.cs
+++ FootballLiveCheck.Service/Controllers/SubscribersController.cs
@@ -1,5 +1,7 @@
 ﻿using FootballLiveCheck.Business.Subscriber.Commands;
 using FootballLiveCheck.Business.Subscriber.Models;
+using FootballLiveCheck.Business.Subscriber.Queries;
+using FootballLiveCheck.Business.Subscriber.QueryResults;
 using FootballLiveCheck.CqrsCore.Dispatchers;
 using FootballLiveCheck.Service.Common;
 using Microsoft.AspNetCore.Mvc;
@@ -17,6 +19,12 @@ namespace FootballLiveCheck.Service.Controllers
         [HttpPost("")]
         public IActionResult CreateTeam([FromBody] SubscriptionModel subscriptionModel)
         {
+            var query = new GetSubscriptionQuery(subscriptionModel);
+            var result = QueryDispatcher.Retrive<GetSubscriptionQueryResult, GetSubscriptionQuery>(query);
+
+            if (result != null)
+                return BadRequest();
+
             var command = new CreateSubscriptionCommand(subscriptionModel);
             CommandDispatcher.Handle(command);
             return Ok();
