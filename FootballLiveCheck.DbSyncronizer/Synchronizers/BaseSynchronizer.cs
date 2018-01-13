975e542b0e2513c43a92832b39c51589529f422b
codrinleonte <codryn_14@yahoo.com>
2018-01-13 17:43:24 +0200

    -changed exxpired api_key

--- FootballLiveCheck.DbSyncronizer/Synchronizers/BaseSynchronizer.cs
+++ FootballLiveCheck.DbSyncronizer/Synchronizers/BaseSynchronizer.cs
@@ -3,6 +3,6 @@
     public class BaseSynchronizer
     {
         protected const string BaseURL = "https://api.crowdscores.com/v1";
-        protected const string APIKey = "48b3efd87bc9456083073f09333558fd";
+        protected const string APIKey = "8821ed583878436784b2c012f9ab680d";
     }
 }
