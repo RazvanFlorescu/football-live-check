13ba47fb7447904c5491d5ce44cc355516cb06bc
rikimoo <razvan_flow@yahoo.com>
2018-01-17 22:07:34 +0200

    added the functionality to link at an endpoint

--- FootballLiveCheck.Web/AFLC/src/app/app.module.ts
+++ FootballLiveCheck.Web/AFLC/src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import {RouterModule} from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -14,7 +16,11 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot([
+      {path: "", component: HomeComponent}  
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- FootballLiveCheck.Web/AFLC/src/app/home/home.component.ts
+++ FootballLiveCheck.Web/AFLC/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Http, Response, Headers} from '@angular/http';
+import 'rxjs/add/operator/toPromise';
 
 @Component({
   selector: 'app-home',
@@ -7,20 +9,105 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-   nrr: number = 0;
-   cuvant: string='liga';
-   strs = [];
+   leagues = [];
+   matches = []; 
+   curentLeagues = [];
+   leagueId:number;
+   show:number=1;
+   homeTeam:string;
+   awayTeam:string;
+   homeGoals:number;
+   awayGoals:number;
+   leagueName:number;
+   homeShirtUrl:string;
+   awayShirtUrl:string;
+   arenaName:string;
+   arenaCapacity:number;
+   email:string;
+   idMatch:number;
+   
+   response:string='Mail was send!'
+   
 
-  constructor() { }
+  constructor(private http: Http) { }
+  id:number;
+  private headers = new Headers({ 'Content-Type': 'application/json'});
+
+
+  getAllLeagues() {
+    this.http.get("http://localhost:5001/api/leagues/topleagues").subscribe(
+      (res: Response) => {
+        this.leagues = res.json();
+        this.curentLeagues = res.json();
+      }
+    )
+  }
+
+  getAllMatches(){
+    this.http.get("http://localhost:5001/api/matches/live").subscribe(
+      (res: Response) => {
+        this.matches = res.json();
+        
+      }
+    )
+    
+  }
+ 
+  getMatchesByLeague(league ){
+    this.http.get("http://localhost:5554/matches").subscribe(
+      (res: Response) => {
+        this.matches = res.json();
+      
+      }
+    )
+    this.show=1;
+  }
+
+  makeSubscribe(homeTeam,awayTeam,homeGoals,awayGoals,leagueName,homeShirtUrl,awayShirtUrl,arenaName,arenaCapacity,idMatch){
+    this.show=0;
+    this.awayTeam=awayTeam;
+    this.homeTeam=homeTeam;
+    this.homeGoals=homeGoals;
+    this.awayGoals=awayGoals;
+    this.leagueName=leagueName;
+    this.homeShirtUrl=homeShirtUrl;
+    this.awayShirtUrl=awayShirtUrl;
+    this.arenaName=arenaName;
+    this.arenaCapacity=arenaCapacity;
+    this.idMatch=idMatch;
+  }
+
+  isAdded: boolean = false;
+  productObj:object = {};
+
+  addSubscribedMatch(matchId,email) {
+    this.productObj = {
+	  "mail" :email,
+	  "subscribedMatch":
+	  {
+ 
+	    "dbid" : matchId
+ 
+	  }
+      
+    }
+  
+    this.http.post("http://localhost:5553/subscribed", this.productObj).subscribe((res:Response) => {
+      this.isAdded = true;
+    })
+    this.show=1;
+  }
 
   ngOnInit() {
     
+    this.getAllMatches();
+    this.getAllLeagues();
+
   }
 
 
-  addItem(){
-    var a = this.cuvant + this.nrr.toString()
-    this.strs.push(a);
-    this.nrr++;
-  }
+
+
+
+
 }
