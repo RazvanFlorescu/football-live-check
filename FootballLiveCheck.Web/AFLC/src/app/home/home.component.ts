9047311f60d7a647d4a5a08c76d061c3287d0796
rikimoo <razvan_flow@yahoo.com>
2018-01-19 10:16:12 +0200

    added final form fronEnd

--- FootballLiveCheck.Web/AFLC/src/app/home/home.component.ts
+++ FootballLiveCheck.Web/AFLC/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Http, Response, Headers} from '@angular/http';
 import 'rxjs/add/operator/toPromise';
+import { forEach } from '@angular/router/src/utils/collection';
 
 @Component({
   selector: 'app-home',
@@ -9,11 +10,14 @@ import 'rxjs/add/operator/toPromise';
 })
 export class HomeComponent implements OnInit {
 
-   leagues = [];
+   leagues =[];
    matches = []; 
-   curentLeagues = [];
+   liveMatches = []; 
+   currentMatches = []; 
+   currentLeagues = [];
    leagueId:number;
    show:number=1;
+   matchId:number;
    homeTeam:string;
    awayTeam:string;
    homeGoals:number;
@@ -22,58 +26,88 @@ export class HomeComponent implements OnInit {
    homeShirtUrl:string;
    awayShirtUrl:string;
    arenaName:string;
-   arenaCapacity:number;
+   arenaCapacity:string;
    email:string;
    idMatch:number;
-   
+   link :string;
+   date:string
    response:string='Mail was send!'
-   
+   subscribedMatches =[];
 
   constructor(private http: Http) { }
   id:number;
-  private headers = new Headers({ 'Content-Type': 'application/json'});
+  private headers = new Headers({ 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'});
 
 
-  getAllLeagues() {
-    this.http.get("http://localhost:10100/leagues").subscribe(
+  getAllLeagues() {   
+    this.http.get("http://localhost:5001/api/leagues/topleagues").subscribe(
       (res: Response) => {
-        this.leagues = res.json();
-        this.curentLeagues = res.json();
+        this.leagues = res.json()['leagues'];
+        this.currentLeagues=res.json()['leagues'];
       }
     )
+             
   }
 
-  getAllMatches(){
-    this.http.get("http://localhost:10101/matches").subscribe(
+  getAllLiveMatches(){
+    this.http.get("http://localhost:5001/api/matches/live").subscribe(
       (res: Response) => {
-        this.matches = res.json();
-        
+        this.liveMatches = res.json()['matches'];
+      
       }
     )
     
   }
  
-  getMatchesByLeague(league ){
-    this.http.get("http://localhost:10101/matches").subscribe(
+  getFullTimeMatchesByLeague(leagueId ){
+    var leagueIdString = ""+leagueId; 
+    this.link = "http://localhost:5001/api/matches/fulltime/"+leagueIdString;
+  
+      this.http.get(this.link).subscribe(
+        (res: Response) => {
+         
+          this.currentMatches = res.json()['matches'];
+        }
+      )
+      this.show=1;
+  }
+  getUpcomingMatchesByLeague(leagueId ){
+    var leagueIdString = ""+leagueId; 
+    this.link = "http://localhost:5001/api/matches/upcoming/"+leagueIdString;
+    this.http.get(this.link).subscribe(
       (res: Response) => {
-        this.matches = res.json();
-      
+       
+        this.currentMatches = res.json()['matches'];
       }
     )
     this.show=1;
   }
 
-  makeSubscribe(homeTeam,awayTeam,homeGoals,awayGoals,leagueName,homeShirtUrl,awayShirtUrl,arenaName,arenaCapacity,idMatch){
+  modifyDate(date){
+    var data = new Date(date);
+    return data.getHours() + ":" + data.getMinutes();
+  }
+
+  
+  
+  makeSubscribe(homeTeam,awayTeam,homeGoals,awayGoals,leagueName,homeShirtUrl,awayShirtUrl,arena,idMatch){
     this.show=0;
     this.awayTeam=awayTeam;
     this.homeTeam=homeTeam;
     this.homeGoals=homeGoals;
     this.awayGoals=awayGoals;
     this.leagueName=leagueName;
+    this.matchId=idMatch;
     this.homeShirtUrl=homeShirtUrl;
     this.awayShirtUrl=awayShirtUrl;
-    this.arenaName=arenaName;
-    this.arenaCapacity=arenaCapacity;
+    if(arena !=null){
+      this.arenaName=arena.name;
+      this.arenaCapacity=arena.capacity;
+    }
+    else{
+      this.arenaName="No data about this arena";
+      this.arenaCapacity="No data about arena capacity";
+    }
     this.idMatch=idMatch;
   }
 
@@ -92,17 +126,56 @@ export class HomeComponent implements OnInit {
       
     }
   
-    this.http.post("http://localhost:5553/subscribed", this.productObj).subscribe((res:Response) => {
+    this.http.post("http://localhost:5001/api/subscribe", this.productObj).subscribe((res:Response) => {
       this.isAdded = true;
     })
     this.show=1;
   }
 
-  ngOnInit() {
+  getAllSubscribedMatches(){
+    this.http.get("http://localhost:5001/api/subscribe").subscribe(
+      (res: Response) => {
+        this.subscribedMatches = res.json()['subscriptions'];
+      }
+    )
     
-    this.getAllMatches();
-    this.getAllLeagues();
+  }
 
+
+  isSubscribedMatch(matchId){
+    for (var i = 0; i < this.subscribedMatches.length; i++) {
+      if(this.subscribedMatches[i].subscribedMatch.dbId == matchId){
+        return true;
+      }
+  }
+  return false;
+  
+  }
+
+
+  getSubscriptionIdByMatchId(matchId){
+    for (var i = 0; i < this.subscribedMatches.length; i++) {
+      if(this.subscribedMatches[i].subscribedMatch.dbId == matchId){
+        return this.subscribedMatches[i].id;
+      }
+    }
+  }
+
+  unSubscribeFromMatch(matchId){
+    var subscriptionId = this.getSubscriptionIdByMatchId(matchId);
+    this.productObj = {
+        "id" : subscriptionId
+    }
+    this.http.delete("http://localhost:5001/api/subscribe",this.productObj).toPromise();
+    this.show=0;
+  }
+
+  
+  ngOnInit() {
+    this.getAllLiveMatches();
+    this.getAllLeagues();
+    this.getAllLiveMatches();
+    this.getAllSubscribedMatches();
   }
 
 
